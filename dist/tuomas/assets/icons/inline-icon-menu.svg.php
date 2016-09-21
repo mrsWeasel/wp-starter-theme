703289fc97e773e2bca1753ad25c8641688f0d9a
mrsWeasel <lauramainen@gmail.com>
2016-09-21 23:57:01 +0300

    Delete dist directory

--- dist/tuomas/assets/icons/inline-icon-menu.svg.php
+++
@@ -1,6 +0,0 @@
-<svg viewBox="0 0 21 20" width="21" height="20">
-	<title><?php esc_html_e('Toggle menu', 'sassyscores') ?></title>
-	<rect id="menu-first" class="icon-element" width="21" height="4"/>
-	<rect id="menu-second" class="icon-element" y="8" width="21" height="4"/>
-	<rect id="menu-third" class="icon-element" y="16" width="21" height="4"/>
-</svg>
