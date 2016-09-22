aa7818eb6003cd5c3e67a88391bc5c7e454f68d4
mrsWeasel <lauramainen@gmail.com>
2016-09-22 21:34:06 +0300

    Deleted not needed assets & scripts

--- assets/icons/inline-icon-down-arrow.svg.php
+++
@@ -1,4 +0,0 @@
-<svg width="45" height="45" viewBox="0 0 45 45">
-	<title><?php esc_html_e('Scroll to main content', 'sassyscores'); ?></title>
-	<polygon class="icon-element" points="44 21.6 23 42.6 23 0.1 22 0.1 22 42.6 0.9 21.6 0.2 22.3 22.5 44.5 44.7 22.3 " />
-</svg>
