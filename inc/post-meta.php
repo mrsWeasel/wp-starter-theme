4fa9f38447adb7e8dca2cb5c133a76a4183dd3ca
mrsWeasel <lauramainen@gmail.com>
2016-09-22 21:08:59 +0300

    Changed function prefixes

--- inc/post-meta.php
+++ inc/post-meta.php
@@ -1,7 +1,7 @@
 <?php
 
-if (! function_exists('tk_entry_meta')) {
-	function tk_entry_meta() {
+if (! function_exists('sassys_entry_meta')) {
+	function sassys_entry_meta() {
 		echo '<section class="entry-meta">';
 
 		echo '<span>' . sprintf( esc_html__('Posted on %1$s by %2$s', 'sassyscores'), get_the_time(), get_the_author() ) . '</span>';
--- inc/tag_cloud.php
+++ inc/tag_cloud.php
@@ -6,13 +6,13 @@
  * @package Sassyscores
  */
 
-add_filter( 'widget_tag_cloud_args', 'tk_tag_cloud_widget');
+add_filter( 'widget_tag_cloud_args', 'sassys_tag_cloud_widget');
 
-if (! function_exists( 'tk_tag_cloud_widget' ) ) {
-	function tk_tag_cloud_widget( $args ) {
+if (! function_exists( 'sassys_tag_cloud_widget' ) ) {
+	function sassys_tag_cloud_widget( $args ) {
 		$args['smallest'] = 14;
 		$args['largest'] = 24;
-		$args['unit'] = 'px';
+		$args['unit'] = 'em';
 		return $args;
 	}
 }	
