5fc010a738573dec9163649cfcf978df2622baba
mrsWeasel <lauramainen@gmail.com>
2016-11-13 22:58:53 +0200

    Remove extra div

--- search.php
+++ search.php
@@ -8,8 +8,6 @@
  */
 
 get_header(); ?>
-	
-	<div class="fullwidth-block">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
 				<div class="container">
@@ -40,7 +38,6 @@ get_header(); ?>
 				</div>
 			</main><!-- #main -->
 		</div><!-- #primary -->
-	</div><!-- .fullwidth-block -->	
 <?php
 
 get_footer();
